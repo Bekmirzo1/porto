6c91737a9e34fddfbe843654423d47e7e33a6a83
Bekmirzo1 <azimovbekmirzo0@gmail.com>
2021-08-19 19:00:41 +0500

    slight changes and optimization

--- js/script.js
+++ js/script.js
@@ -55,64 +55,22 @@ if (animItems.length > 0) {
         animOnScroll();
     }, 300);
 };
-const isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function () {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-};
-if (!isMobile.any()) {
-    document.body.classList.add('_pc')
-}
 
-const sliders = document.querySelectorAll('.slider');
-if (sliders.length > 0) {
-    for (let index = 0; index < sliders.length; index++) {
-        const slider = sliders[index];
-        let mySwiper;
-        window.addEventListener('resize', mobileSlider);
-        function mobileSlider() {
-            if (document.documentElement.clientWidth >= 767 && slider.dataset.mobile == 'true') {
-                mySwiper = new Swiper(slider, {
-                    navigation: {
-                        nextEl: '._icon-next',
-                        prevEl: '._icon-next-r'
-                    },
-                    effect: 'fade',
-                    fadeEffect: {
-                        // Параллельная смена прозрачности
-                        crossFade: true
-                    },
-                    loop: false,
-                    slidesPerView: 1,
-                    simulateTouch: false,
-                });
-                slider.dataset.mobile = 'false';
-            } else if (document.documentElement.clientWidth < 767) {
-                slider.dataset.mobile = 'true'
-                if (slider.classList.contains('swiper-container-initialized')) {
-                    mySwiper.destroy()
-                }
-            }
-        }
-        mobileSlider();
-    }
+const slider = document.querySelector('.slider');
+if (slider) {
+    new Swiper(slider, {
+        navigation: {
+            nextEl: '._icon-next',
+            prevEl: '._icon-next-r'
+        },
+        effect: 'fade',
+        fadeEffect: {
+            // Параллельная смена прозрачности
+            crossFade: true
+        },
+        slidesPerView: 1,
+    });
 }
-
 
 // footerIcon up
 const footerIcon = document.querySelector('.footer__icon');
